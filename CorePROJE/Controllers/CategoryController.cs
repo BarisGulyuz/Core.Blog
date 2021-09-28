bbdba43e817d551a9b57bb25979bfc13d4ba33e2
BarisGulyuz <bar.77@windowslive.com>
2021-09-28 16:59:16 +0300

    Add project files.

---
+++ CorePROJE/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.Concrete;
+using BussinessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePROJE.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
